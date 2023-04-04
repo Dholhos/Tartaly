19f15caaa9dcd519893455c309cca6a2abf00545
Dholhos <113104720+Dholhos@users.noreply.github.com>
2023-04-04 22:26:40 +0200

    Add files via upload

--- Tartaly/MainWindow.xaml.cs
+++ Tartaly/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Osztályok;
 
 namespace Tartaly
 {
@@ -23,6 +24,53 @@ namespace Tartaly
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void rdoTeglatest_Checked(object sender, RoutedEventArgs e)
+        {
+            txtAel.IsEnabled = true;
+            txtBel.IsEnabled = true;
+            txtCel.IsEnabled = true;
+            txtAel.Text = "";
+            txtBel.Text = "";
+            txtCel.Text = "";
+        }
+
+        private void rdoKocka_Checked(object sender, RoutedEventArgs e)
+        {
+            txtAel.IsEnabled = false;
+            txtBel.IsEnabled = false;
+            txtCel.IsEnabled = false;
+            txtAel.Text = "10";
+            txtBel.Text = "10";
+            txtCel.Text = "10";
+        }
+        private void btnFelvesz_Click(object sender, RoutedEventArgs e)
+        {
+            Tartaly ujTest;
+
+            ujTest = new Tartaly(txtNev.Text, Convert.ToInt32(txtAel.Text), Convert.ToInt32(txtBel.Text), Convert.ToInt32(txtCel.Text));
+            lbTartalyok.Items.Add(ujTest.Info());
+            tartalyok.Add(ujTest);
+        }
+        private void btntolt_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbTartalyok.SelectedIndex >= 0)
+            {
+                tartalyok[lbTartalyok.SelectedIndex].Tolt(Convert.ToDouble(txtMennyitTolt.Text));
+                lbTartalyok.Items[lbTartalyok.SelectedIndex] = tartalyok[lbTartalyok.SelectedIndex].Info();
+
+            }
+        }
+
+        private void btnDuplaz_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbTartalyok.SelectedIndex >= 0)
+            {
+                tartalyok[lbTartalyok.SelectedIndex].DuplazMeretet();
+                lbTartalyok.Items[lbTartalyok.SelectedIndex] = tartalyok[lbTartalyok.SelectedIndex].Info();
+
+            }
         }
     }
-}
